f5457e7282624ebe8409e54d8f1b14552a2e6f1a
sayani <sayani.pramanik004@gmail.com>
2024-09-14 13:12:50 +0530

    INItial Push

--- cypress/e2e/5.Custom-Command/1-Custom_commands.cy.js
+++ cypress/e2e/5.Custom-Command/1-Custom_commands.cy.js
@@ -1,11 +1,11 @@
-describe('test',()=>{
-    it('Custom commands' , ()=>{
+describe('test', () => {
+    it('Custom commands', () => {
         cy.visit('/')
-        cy.login(Cypress.env('Email'),Cypress.env('Pass'))  //custom commands
+        cy.login(Cypress.env('Email'), Cypress.env('Pass'))  //custom commands
         cy.Wishlist() // custom command
         cy.get('div#bar-notification.bar-notification.success').should('be.visible')
         cy.get('a.ico-wishlist').first().click()
-        cy.get('tbody').children('tr.cart-item-row').should('have.length.greaterThan',0)
+        cy.get('tbody').children('tr.cart-item-row').should('have.length.greaterThan', 0)
         cy.screenshot('custom-commands')
     })
 })
