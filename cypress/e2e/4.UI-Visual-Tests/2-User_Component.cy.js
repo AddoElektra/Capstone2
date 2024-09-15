61d050f86a8a138087988fe2f1c04a201122660a
sayani <sayani.pramanik004@gmail.com>
2024-09-15 10:49:40 +0530

    initial commit

--- cypress/e2e/4.UI-Visual-Tests/2-User_Component.cy.js
+++ cypress/e2e/4.UI-Visual-Tests/2-User_Component.cy.js
@@ -1,13 +1,13 @@
-describe('test',()=>{
-    it('login',()=>{
+describe('test', () => {
+    it('User Render Correctly', () => {
         cy.visit('/')
-        cy.login(Cypress.env('Email'),Cypress.env('Pass'))
+        cy.login(Cypress.env('Email'), Cypress.env('Pass'))
         cy.get('a.account').first().click()
         cy.get('input#FirstName').should('have.value', Cypress.env('FName')).and('be.visible')
-        .screenshot('First-Name')
+            .screenshot('First-Name')
         cy.get('input#LastName').should('have.value', Cypress.env('LName')).and('be.visible')
-        .screenshot('Last-Name')
+            .screenshot('Last-Name')
         cy.get('input#Email').should('have.value', Cypress.env('Email')).and("be.visible")
-        .screenshot('Email')
+            .screenshot('Email')
     })
 })
