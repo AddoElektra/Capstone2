97d3420ad64dcf649e900257156f92abd28ac27c
sayani <sayani.pramanik004@gmail.com>
2024-09-15 10:46:48 +0530

    Initial commit

--- cypress/e2e/2.Functional-Tests/3-Registration.cy.js
+++ cypress/e2e/2.Functional-Tests/3-Registration.cy.js
@@ -1,5 +1,5 @@
 describe('test',()=>{
-    it('Register',()=>{
+    it('Registration',()=>{
         cy.visit('/')
         cy.get('a.ico-register').click()
         cy.get('input#gender-female').check()
@@ -11,9 +11,17 @@ describe('test',()=>{
         cy.screenshot('filling-form')
         cy.get('input#register-button').click()
         cy.get('div.page-title').should('contain','Register')
-        cy.get('div.result').should('contain','Your registration completed')
-        cy.screenshot('registration')
-        cy.get('input.button-1.register-continue-button').click()
-        cy.get('div.header-links ul').children('li').first().should('contain',Cypress.env('REmail'))
+        cy.get('div.page-body').then(($result) => {
+            if ($result.text().includes('The specified email already exists')) {
+              cy.log('Email already exists, aborting test')
+              cy.screenshot('email-exists')
+              cy.end() // abort the test
+            } else {
+              cy.get('div.result').should('contain', 'Your registration completed')
+              cy.screenshot('registration')
+              cy.get('input.button-1.register-continue-button').click()
+              cy.get('div.header-links ul').children('li').first().should('contain', Cypress.env('REmail'))
+            }
+          })
     })
 })
